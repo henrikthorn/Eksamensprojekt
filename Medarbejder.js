738917fabd3b645a93a1d2f698113835e84f521a
Benhn12 <ben.hn12@gmail.com>
2019-10-14 09:27:18 +0200

    Medarbejder class opdatering

--- Medarbejder.js
+++ Medarbejder.js
@@ -1,3 +1,10 @@
 class medarbejder{
-
+constructor(employee_user, phone, department, yy, sex, skill){
+  this.employee_user = employee_user;
+    this.phone = phone;
+    this.department = department;
+    this.yy = yy;
+    this.sex = sex;
+    this.skill = skill;
+}
 }
