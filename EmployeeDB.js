15701fea70d12d26ca6f29b7d1b56db5abeade1e
Benhn12 <ben.hn12@gmail.com>
2019-10-14 10:29:25 +0200

    Medarbejder class opdatering

---
+++ EmployeeDB.js
@@ -0,0 +1,6 @@
+
+var medarbejder1 = new medarbejder("Simon", 12345678, "HR", 1999,"Male", "Mangement");
+var medarbejder2 = new medarbejder ("Mads", 87654321,"IT", 1999, "Male", "Front-end");
+var medarbejder3 = new medarbejder("Jessica", 252522525, "Sales",1998, "Female","Sales");
+var medarbejder4 = new medarbejder("Benjamin", 60504497,"IT", 1997, "Male", "Back-end");
+
