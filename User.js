423d3d41507f1b6326ca731418073da991409fbc
Simon Mathiesen <sima19ae@student.cbs.dk>
2019-10-15 09:34:32 +0200

    Simon

--- User.js
+++ User.js
@@ -1,42 +1,37 @@
-window.addEventListener("DOMContentLoaded", setupForm);
+var existingEmployee = JSON.parse(localStorage.getItem('Employee'));
+var submit = document.getElementById('submit');
+var currentLogin = [];
 
-function setupForm(){
-    const form = document.getElementById("login-form");
-    form.addEventListener("submit", handleSubmit);
-}//setupForm
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+submit.onclick = function(){
 
-function handleSubmit(event){
-    event.preventDefault();
+    var usernameInput = document.getElementById("username").value;
+    var passwordInput = document.getElementById("password").value;
 
-    const {
-        username,
-        password,
-        login_type,
-        result
-    } = event.target;
-
-    const processUser = login_type.value == "register" ? registerUser : loginUser;
-    const response    = processUser(username.value, password.value);
-    result.innerHTML  = response;
-}//handleSubmit
-
-function registerUser(username, password){
-    window.localStorage.setItem("exampleLogin__username", username);
-    window.localStorage.setItem("exampleLogin__password", password);
-
-    return `New user ${username} now registered!`;
-}//registerUser
-
-function loginUser(username, password){
-    const registeredUser     = window.localStorage.getItem("exampleLogin__username");
-    const registeredPassword = window.localStorage.getItem("exampleLogin__password");
-
-    const validUser     = username == registeredUser;
-    const validPassword = password == registeredPassword;
-
-    if(validUser && validPassword){
-        return `User ${username} successfully logged-in!`;
+    for (let i = 0;i < existingEmployee.length; i++) {
+        if ( usernameInput == existingEmployee[i].username && passwordInput == existingEmployee[i].password){
+            currentLogin.push({employee_username: usernameInput})
+            document.location.href ='succes.html';
+            var IDString = JSON.stringify(currentLogin);
+            localStorage.setItem('currentEmployee', IDString);
+            alert ("Login successfully");
+            return true;
+         }
+    else{
+        attempt --;// Decrementing by one.
+        alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+        if( attempt == 0){
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
     }
-    else if(!validUser)     return `Username ${username} has not been registered. `;
-    else if(!validPassword) return `Incorrect password for username ${username}`;
-}//loginUser
+    }
+}
+
+
+
+
